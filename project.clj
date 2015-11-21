428936bbe0a385a3b081bfc0aeface59b9769f08
suppi <soupiral@gmail.com>
2015-11-21 16:53:55 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject slides "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An EDSL for creating slideshows in ClojureScript"
+  :url "https://github.com/soupi/slides"
   :license {:name "BSD3"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
